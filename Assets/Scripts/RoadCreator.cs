7e28f2deb1c69dda8471e67e882e9bd32cba76f7
Maximiliano Guerra de Medeiros <maxciencomp@gmail.com>
2016-01-30 17:19:07 -0300

    Added holes to the road, removed randomness of road tiles, will draw obstacles on top

--- Assets/Scripts/RoadCreator.cs
+++ Assets/Scripts/RoadCreator.cs
@@ -6,33 +6,40 @@ public class RoadCreator : MonoBehaviour {
 	public GameObject firstTile;
 	public float endOfRoadX;
 	//public GameObject camera;
-	public GameObject[] tiles;
-
+	public GameObject roadTile;
+	public float tileSize;
+	public bool lastTileWasEmpty = false;
+	public int totalTiles = 0;
 
 	void Start () {
-		//GameObject road = GameObject.Find("Road");
-
 		if (endOfRoadX == 0) {
-			//Bounds roadBounds = road.GetComponent<MeshRenderer>().bounds;
-		//	Bounds roadBounds = road.GetComponent<BoxCollider>().bounds;
-		//	Vector3 endOfRoad = new Vector3(roadBounds.center.x - roadBounds.extents.x, 0, 0);
-
 			Vector3 endOfRoad = new Vector3(firstTile.GetComponent<BoxCollider>().center.x - firstTile.GetComponent<BoxCollider>().size.x, 0, 0);
+			tileSize = firstTile.GetComponent<BoxCollider>().size.x;
 		}
-
 	}
 
 	void drawTile() {
-		Vector3 newPosition = new Vector3(endOfRoadX, 0, 0);
-		GameObject randomTile = tiles[Random.Range(0, tiles.Length)];
-		GameObject tileInstance = (GameObject)Instantiate(randomTile, newPosition, Quaternion.identity);
-		endOfRoadX -= randomTile.GetComponent<BoxCollider>().size.x;
+		int chanceOfHole = Random.Range(0, 100);
+		int chanceOfObstacle = Random.Range(0, 100);
+
+		Debug.Log("Chance of Hole: " + chanceOfHole);
+
+		if (chanceOfHole <= 30 && lastTileWasEmpty == false && totalTiles > 10) {
+			endOfRoadX -= tileSize;
+			lastTileWasEmpty = true;
+		} else if (chanceOfObstacle <= 30 && totalTiles > 10) {
+			
+		} else {
+			Vector3 newPosition = new Vector3(endOfRoadX, 0, 0);
+			GameObject tileInstance = (GameObject)Instantiate(roadTile, newPosition, Quaternion.identity);
+			endOfRoadX -= tileInstance.GetComponent<BoxCollider>().size.x;
+			lastTileWasEmpty = false;
+		}
+		totalTiles += 1;
 	}
 
 	void Update () {
-		
-		if ((Camera.main.transform.localPosition.x - ((Camera.main.GetComponent<Camera>().orthographicSize*3))) < endOfRoadX ) 
-    	{
+		if ((Camera.main.transform.localPosition.x - ((Camera.main.GetComponent<Camera>().orthographicSize * 3))) < endOfRoadX ) {
 			drawTile();
 		}
 	}
