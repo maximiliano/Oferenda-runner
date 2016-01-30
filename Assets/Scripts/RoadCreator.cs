fb336c56886fe40acdb9f6644c415f15c6e7c796
Maximiliano Guerra de Medeiros <maxciencomp@gmail.com>
2016-01-30 17:31:30 -0300

    Removed firstTile from road creator script

--- Assets/Scripts/RoadCreator.cs
+++ Assets/Scripts/RoadCreator.cs
@@ -3,23 +3,19 @@ using System.Collections;
 
 public class RoadCreator : MonoBehaviour {
 
-	public GameObject firstTile;
 	public float endOfRoadX;
-	//public GameObject camera;
 	public GameObject roadTile;
 	public float tileSize;
 	public bool lastTileWasEmpty = false;
 	public int totalTiles = 0;
 
 	void Start () {
-		tileSize = firstTile.GetComponent<BoxCollider>().size.x;
+		tileSize = roadTile.GetComponent<BoxCollider>().size.x;
 	}
 
 	void drawTile() {
 		int chanceOfHole = Random.Range(0, 100);
 		int chanceOfObstacle = Random.Range(0, 100);
-
-		Debug.Log("Chance of Hole: " + chanceOfHole);
 
 		if (chanceOfHole <= 30 && lastTileWasEmpty == false && totalTiles > 10) {
 			endOfRoadX -= tileSize;
