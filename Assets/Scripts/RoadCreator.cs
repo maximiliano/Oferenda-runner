c9e8a08ce55deb218fe990507d75017f41cf11d6
Maximiliano Guerra de Medeiros <maxciencomp@gmail.com>
2016-01-30 17:28:16 -0300

    Refactored unused code

--- Assets/Scripts/RoadCreator.cs
+++ Assets/Scripts/RoadCreator.cs
@@ -12,10 +12,7 @@ public class RoadCreator : MonoBehaviour {
 	public int totalTiles = 0;
 
 	void Start () {
-		if (endOfRoadX == 0) {
-			Vector3 endOfRoad = new Vector3(firstTile.GetComponent<BoxCollider>().center.x - firstTile.GetComponent<BoxCollider>().size.x, 0, 0);
-			tileSize = firstTile.GetComponent<BoxCollider>().size.x;
-		}
+		tileSize = firstTile.GetComponent<BoxCollider>().size.x;
 	}
 
 	void drawTile() {
@@ -27,12 +24,10 @@ public class RoadCreator : MonoBehaviour {
 		if (chanceOfHole <= 30 && lastTileWasEmpty == false && totalTiles > 10) {
 			endOfRoadX -= tileSize;
 			lastTileWasEmpty = true;
-		} else if (chanceOfObstacle <= 30 && totalTiles > 10) {
-			
 		} else {
 			Vector3 newPosition = new Vector3(endOfRoadX, 0, 0);
 			GameObject tileInstance = (GameObject)Instantiate(roadTile, newPosition, Quaternion.identity);
-			endOfRoadX -= tileInstance.GetComponent<BoxCollider>().size.x;
+			endOfRoadX -= tileSize;
 			lastTileWasEmpty = false;
 		}
 		totalTiles += 1;
