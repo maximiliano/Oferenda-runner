f3899d202e0f996910c6f82555684b5261f22a47
Maximiliano Guerra de Medeiros <maxciencomp@gmail.com>
2016-01-31 15:28:37 -0300

    Fixed avatar collision with obstacle, father doesn't get close if avatar is on top

--- Assets/Scripts/Avatar.cs
+++ Assets/Scripts/Avatar.cs
@@ -22,30 +22,11 @@ public class Avatar : MonoBehaviour {
 		doubleJumped = false;
 
 		if (collision.gameObject.CompareTag("Obstacle")) {
-			collidedObstacle = true;
+			float topOfObstacle = collision.transform.position.y + collision.gameObject.GetComponent<BoxCollider>().size.y / 2;
+			if (this.gameObject.transform.position.y < topOfObstacle) {
+				collidedObstacle = true;
+			}
 		}
-
-//		void OnTriggerEnter(Collider other) {
-//		if (other.gameObject.CompareTag("Avatar")) {
-//			GameManager managerScript = GameObject.Find("GameManager").GetComponent<GameManager>();
-//			managerScript.score += 1;
-//			GameObject scoreBoard = GameObject.Find("ScoreBoard");
-//			scoreBoard.GetComponent<Text>().text = "Score: " + managerScript.score;
-//
-//			GameObject pai = GameObject.Find("Pai");
-//			pai.GetComponent<CameraControl2D>().offSet.x += 0.5f;
-//
-//			if ((Camera.main.transform.localPosition.x + ((Camera.main.GetComponent<Camera>().orthographicSize*2.2))) < pai.transform.position.x) {
-//				managerScript.playerWon = true;
-//				other.gameObject.GetComponent<Avatar>().speed = 0;
-//				other.gameObject.GetComponent<Rigidbody>().isKinematic = true;
-//				Destroy(pai);
-//			}
-//
-//			Destroy(this.gameObject);
-//		}
-//	}
-//
 	}
 
 	void OnCollisionStay(Collision collision) {
