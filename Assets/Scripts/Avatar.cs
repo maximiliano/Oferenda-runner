1d82c525be3a9fd53ff412ec7af0a0c71736e18a
Maximiliano Guerra de Medeiros <maxciencomp@gmail.com>
2016-01-30 22:28:43 -0300

    Added double jump to avatar

--- Assets/Scripts/Avatar.cs
+++ Assets/Scripts/Avatar.cs
@@ -5,6 +5,7 @@ public class Avatar : MonoBehaviour {
 	public int speed;
 	public int jumpingForce;
 	public bool isGrounded;
+	public bool doubleJumped = false;
 
 	// Use this for initialization
 
@@ -16,33 +17,34 @@ public class Avatar : MonoBehaviour {
 
 
 	void OnCollisionEnter(Collision collision) {
-
 		isGrounded = true;
-
+		doubleJumped = false;
 	}
 
 	void OnCollisionStay(Collision collision) {
-
 		isGrounded = true;
-
 	}
 
 	void OnCollisionExit(Collision collision) {
-
 		isGrounded = false;
-
 	}
 
 	// Update is called once per frame
 	void Update () {
 
-		_animator.SetBool("Ground",isGrounded);
+		_animator.SetBool("Ground", isGrounded);
 
-		if(isGrounded )
-		if (Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.Mouse0)) {
+		bool enteredJumpInput = Input.GetKeyDown(KeyCode.UpArrow) || 
+			                    Input.GetKeyDown(KeyCode.Space) || 
+			                    Input.GetKeyDown(KeyCode.Mouse0);
+		if (isGrounded && enteredJumpInput) {
 			this.gameObject.GetComponent<Rigidbody>().AddForce(Vector3.up * jumpingForce);
 		}
-		
+		if (isGrounded == false && doubleJumped == false && enteredJumpInput) {
+			this.gameObject.GetComponent<Rigidbody>().AddForce(Vector3.up * jumpingForce/2);
+			doubleJumped = true;
+		}
+
 		this.transform.position = new Vector3(this.transform.position.x - speed * Time.deltaTime,
 		                                      this.transform.position.y, 
 		                                      this.transform.position.z);
